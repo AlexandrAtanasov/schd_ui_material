e1a542d887a5a016f4c0359f5d10b215d571a47b
AtanasovAlexandr <mephistosaa@gmail.com>
2020-09-03 20:44:55 +0300

    update styles for intro page

--- Components/MainIndexButtons/MainIndexButtonsComponent.js
+++ Components/MainIndexButtons/MainIndexButtonsComponent.js
@@ -1,25 +1,10 @@
 import Link from 'next/link' 
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
 
 export default function BasicButtonGroup() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div className='intro_page'>
         <Link href="/first/SCHD">
             <a>
                 <Button  variant="contained" color="primary">
